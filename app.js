8cab0933e47fd17471fdaab6a6f3fb98ad91dc0d
Camilo Giraldo <camilogiraldo91@gmail.com>
2017-01-13 17:37:29 -0500

    add un/subscribe routes

--- app.js
+++ app.js
@@ -172,6 +172,29 @@ app.get('/api/shows/:id', function(req, res, next) {
     })
 })
 
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next){
+  Show.findById(req.body.showId, function(err, show){
+    if (err) return next(err);
+    show.subscribers.push(req.user.id);
+    show.save(function(err){
+      res.send(200);
+    })
+  })
+})
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next){
+  Show.findById(req.body.showId, function(err, show){
+    if (err) return next(err);
+    var index = show.subscribers.indexOf(req.user.id);
+    show.subscribers.splice(index, 1);
+    show.save(function(err){
+      if (err) return next(err);
+      res.send(200);
+    })
+  })
+})
+
+
 app.post('/api/shows', function(req, res, next) {
 
     var apiKey = '9EF1D1E7D28FDA0B';
